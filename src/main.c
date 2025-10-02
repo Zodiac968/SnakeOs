c0d0573966ed07a538b337c5ef949bf4e66e832a
Mohamed Reihan <mohamedreihan968@gmail.com>
2025-10-02 14:34:16 +0530

    Added a splash screen for snake os

--- src/main.c
+++ src/main.c
@@ -2,6 +2,32 @@
 #include <terminal_gop.h>
 // ==== Function prototypes ====
 
+// 688*320
+const UINTN logo_width = 688;
+const UINTN logo_height = 320;
+static const CHAR16 *SnakeOsLogo[] = {
+    L"                                                                                      ",
+    L"      #######                          /                          # ###               ",
+    L"    /       ###                      #/                         /  /###              ",
+    L"   /         ##                      ##                        /  /  ###             ",
+    L"   ##        #                       ##                       /  ##   ###            ",
+    L"    ###                              ##                      /  ###    ###           ",
+    L"   ## ###      ###  /###     /###    ##  /##      /##       ##   ##     ##    /###   ",
+    L"    ### ###     ###/ #### / / ###  / ## / ###    / ###      ##   ##     ##   / #### /",
+    L"      ### ###    ##   ###/ /   ###/  ##/   /    /   ###     ##   ##     ##  ##  ###/ ",
+    L"        ### /##  ##    ## ##    ##   ##   /    ##    ###    ##   ##     ## ####      ",
+    L"          #/ /## ##    ## ##    ##   ##  /     ########     ##   ##     ##   ###     ",
+    L"           #/ ## ##    ## ##    ##   ## ##     #######       ##  ##     ##     ###   ",
+    L"            # /  ##    ## ##    ##   ######    ##             ## #      /        ### ",
+    L"  /##        /   ##    ## ##    /#   ##  ###   ####    /       ###     /    /###  ## ",
+    L" /  ########/    ###   ### ####/ ##  ##   ### / ######/         ######/    / #### /  ",
+    L"/     #####       ###   ### ###   ##  ##   ##/   #####            ###         ###/   ",
+    L"|                                                                                     ",
+    L" \\)                                                                                  ",
+    L"                                                                                      ",
+    L"                                                                                      ",
+};
+
 EFI_STATUS
 EFIAPI
 efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
@@ -27,6 +53,23 @@ efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
         UINTN width = gop->Mode->Info->HorizontalResolution;
         UINTN height = gop->Mode->Info->VerticalResolution;
         EFI_INPUT_KEY key;
+        UINTN yPos = -logo_height; // actual Pos = -logo height (top left corner)
+        UINTN delay = 1000000 / 10;
+        while (1)
+        {
+                for (UINTN i = 0; i < sizeof(SnakeOsLogo) / sizeof(SnakeOsLogo[0]); i++)
+                {
+                        draw_string(framebuffer, width, (width - logo_width) / 2, yPos + i * 16, SnakeOsLogo[i], 0xFFFFFFFF);
+                }
+                if (yPos == (height - logo_height) / 2 / 8 * 8)
+                {
+                        BS->Stall(delay * 5);
+                        break;
+                }
+                BS->Stall(delay);
+                yPos += 16;
+        }
+
         InitializeTerminal(framebuffer, 1000, 600, width, height);
 
         DrawBorder(framebuffer, 0xFFFFFFFF);
