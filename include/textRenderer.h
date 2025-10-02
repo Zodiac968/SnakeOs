257e1ae07eef19f1afb109c1009a62393eeea0ee
Mohamed Reihan <mohamedreihan968@gmail.com>
2025-10-02 14:33:52 +0530

    added function clearScreen

--- include/textRenderer.h
+++ include/textRenderer.h
@@ -4,6 +4,7 @@
 #include <efi.h>
 extern unsigned char vgafont[4096];
 extern unsigned int vgafont_len;
+void clearScreen(UINT32 *framebuffer, UINTN fb_width, UINTN fb_height, UINT32 color);
 void draw_char(UINT32 *framebuffer, UINTN fb_width, UINTN x, UINTN y, unsigned char *glyph, UINT32 color);
 void draw_string(UINT32 *framebuffer, UINTN fb_width, UINTN x, UINTN y, const CHAR16 *str, UINT32 color);
 void draw_all_chars(UINT32 *framebuffer, UINTN fb_width, UINTN x, UINTN y, UINT32 color);
